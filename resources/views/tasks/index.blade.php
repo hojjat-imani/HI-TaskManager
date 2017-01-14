0b83bb0a167b0572d166635bb57613d2eb03a746
Hojjat <imani.hojjat95@gmail.com>
2017-01-14 13:19:08 +0330

    add desc field in task view

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="col-sm-offset-2 col-sm-8">
             <div class="panel panel-default">
                 <div class="panel-heading">
@@ -10,18 +10,19 @@
 
                 <div class="panel-body">
                     <!-- Display Validation Errors -->
-                    @include('common.errors')
+                @include('common.errors')
 
-                    <!-- New Task Form -->
+                <!-- New Task Form -->
                     <form action="{{ url('task') }}" method="POST" class="form-horizontal">
-                        {{ csrf_field() }}
+                    {{ csrf_field() }}
 
-                        <!-- Task Name -->
+                    <!-- Task Name -->
                         <div class="form-group">
                             <label for="task-name" class="col-sm-3 control-label">Task</label>
 
                             <div class="col-sm-6">
-                                <input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
+                                <input type="text" name="name" id="task-name" class="form-control"
+                                       value="{{ old('task') }}">
                             </div>
                         </div>
 
@@ -38,41 +39,88 @@
             </div>
 
             <!-- Current Tasks -->
-            @if (count($tasks) > 0)
+            {{--@if (count($tasks) > 0)--}}
+                {{--<div class="panel panel-default">--}}
+                    {{--<div class="panel-heading">--}}
+                        {{--Current Tasks--}}
+                    {{--</div>--}}
+
+                    {{--<div class="panel-body">--}}
+
+                        {{--<table class="table table-striped task-table">--}}
+                        {{--<thead>--}}
+                        {{--<th>Task</th>--}}
+                        {{--<th>Description</th>--}}
+                        {{--<th>&nbsp;</th>--}}
+                        {{--</thead>--}}
+                        {{--<tbody>--}}
+                        {{--@foreach ($tasks as $task)--}}
+                        {{----}}
+                        {{--<tr>--}}
+                        {{--<td class="table-text">--}}
+                        {{--<div>{{ $task->name }}</div>--}}
+                        {{--</td>--}}
+                        {{--<td class="table-text">--}}
+                        {{--<div>{{ $task->desc }}</div>--}}
+                        {{--</td>--}}
+
+                        {{--<!-- Task Delete Button -->--}}
+                        {{--<td>--}}
+                        {{--<form action="{{url('task/' . $task->id)}}" method="POST">--}}
+                        {{--{{ csrf_field() }}--}}
+                        {{--{{ method_field('DELETE') }}--}}
+
+                        {{--<button type="submit" id="delete-task-{{ $task->id }}"--}}
+                        {{--class="btn btn-danger">--}}
+                        {{--<i class="fa fa-btn fa-trash"></i>Delete--}}
+                        {{--</button>--}}
+                        {{--</form>--}}
+                        {{--</td>--}}
+                        {{--</tr>--}}
+                        {{--@endforeach--}}
+                        {{--</tbody>--}}
+                        {{--</table>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--@endif--}}
+        </div>
+
+        <div class="row">
+            {{--for each task list--}}
+            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        Current Tasks
+                        TaskListName and AddTaskButton
                     </div>
-
                     <div class="panel-body">
-                        <table class="table table-striped task-table">
-                            <thead>
-                                <th>Task</th>
-                                <th>&nbsp;</th>
-                            </thead>
-                            <tbody>
-                                @foreach ($tasks as $task)
-                                    <tr>
-                                        <td class="table-text"><div>{{ $task->name }}</div></td>
-
-                                        <!-- Task Delete Button -->
-                                        <td>
-                                            <form action="{{url('task/' . $task->id)}}" method="POST">
-                                                {{ csrf_field() }}
-                                                {{ method_field('DELETE') }}
-
-                                                <button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger">
-                                                    <i class="fa fa-btn fa-trash"></i>Delete
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                        @if(count($tasks) > 0)
+                            @foreach($tasks as $task)
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        {{$task->name}}
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <input type="text" form="editTask" name="desc" value="{{$task->desc}}">
+                                            </div>
+                                        </div>
+                                        <form id="editTask" action="{{url('task/' . $task->id)}}" method="POST">
+                                            {{csrf_field()}}
+                                        </form>
+                                        <form id="deleteTask" action="{{url('task/' . $task->id)}}" method="POST">
+                                            {{csrf_field()}}
+                                            {{method_field('DELETE')}}
+                                        </form>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @else
+                            No task yet.
+                        @endif
                     </div>
                 </div>
-            @endif
+            </div>
         </div>
     </div>
 @endsection
