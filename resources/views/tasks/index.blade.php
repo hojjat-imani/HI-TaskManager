fbe53336ab617d4698706b07a90552cc241714f9
Hojjat <imani.hojjat95@gmail.com>
2017-01-19 13:45:00 +0330

    trying to show task_lists properly

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,42 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="col-sm-offset-2 col-sm-8">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    New Task
-                </div>
+    <div class="container-fluid container-scroll">
+        <div class="row">
+            <div class="col-sm-12">
+                {{--<div class="panel panel-default">--}}
+                    {{--<div class="panel-heading">--}}
+                        {{--New Task--}}
+                    {{--</div>--}}
 
-                <div class="panel-body">
-                    <!-- Display Validation Errors -->
-                @include('common.errors')
+                    {{--<div class="panel-body">--}}
+                        <!-- Display Validation Errors -->
+                    @include('common.errors')
 
-                <!-- New Task Form -->
-                    <form action="{{ url('task') }}" method="POST" class="form-horizontal">
-                    {{ csrf_field() }}
+                    <!-- New Task Form -->
+                        <form action="{{ url('task') }}" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
 
-                    <!-- Task Name -->
-                        <div class="form-group">
-                            <label for="task-name" class="col-sm-3 control-label">Task</label>
+                        <!-- Task Name -->
+                            <div class="form-group">
+                                <label for="task-name" class="col-sm-3 control-label">Task</label>
 
-                            <div class="col-sm-6">
-                                <input type="text" name="name" id="task-name" class="form-control"
-                                       value="{{ old('task') }}">
+                                <div class="col-sm-6">
+                                    <input type="text" name="name" id="task-name" class="form-control"
+                                           value="{{ old('task') }}">
+                                </div>
                             </div>
-                        </div>
 
-                        <!-- Add Task Button -->
-                        <div class="form-group">
-                            <div class="col-sm-offset-3 col-sm-6">
-                                <button type="submit" class="btn btn-default">
-                                    <i class="fa fa-btn fa-plus"></i>Add Task
-                                </button>
+                            <!-- Add Task Button -->
+                            <div class="form-group">
+                                <div class="col-sm-offset-3 col-sm-6">
+                                    <button type="submit" class="btn btn-default">
+                                        <i class="fa fa-btn fa-plus"></i>Add Task
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
-                </div>
+                        </form>
+                    {{--</div>--}}
+                {{--</div>--}}
             </div>
+        </div>
+        {{--<div class="col-sm-offset-2 col-sm-8">--}}
+
 
             <!-- Current Tasks -->
             {{--@if (count($tasks) > 0)--}}
@@ -83,11 +88,116 @@
                     {{--</div>--}}
                 {{--</div>--}}
             {{--@endif--}}
-        </div>
+        {{--</div>--}}
 
         <div class="row">
             {{--for each task list--}}
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <div class="col col-lg-4 col-md-4 col-sm-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        TaskListName and AddTaskButton
+                    </div>
+                    <div class="panel-body">
+                        @if(count($tasks) > 0)
+                            @foreach($tasks as $task)
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        {{$task->name}}
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <input type="text" form="editTask" name="desc" value="{{$task->desc}}">
+                                            </div>
+                                        </div>
+                                        <form id="editTask" action="{{url('task/' . $task->id)}}" method="POST">
+                                            {{csrf_field()}}
+                                        </form>
+                                        <form id="deleteTask" action="{{url('task/' . $task->id)}}" method="POST">
+                                            {{csrf_field()}}
+                                            {{method_field('DELETE')}}
+                                        </form>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @else
+                            No task yet.
+                        @endif
+                    </div>
+                </div>
+            </div>
+
+            <div class="col col-lg-4 col-md-4 col-sm-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        TaskListName and AddTaskButton
+                    </div>
+                    <div class="panel-body">
+                        @if(count($tasks) > 0)
+                            @foreach($tasks as $task)
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        {{$task->name}}
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <input type="text" form="editTask" name="desc" value="{{$task->desc}}">
+                                            </div>
+                                        </div>
+                                        <form id="editTask" action="{{url('task/' . $task->id)}}" method="POST">
+                                            {{csrf_field()}}
+                                        </form>
+                                        <form id="deleteTask" action="{{url('task/' . $task->id)}}" method="POST">
+                                            {{csrf_field()}}
+                                            {{method_field('DELETE')}}
+                                        </form>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @else
+                            No task yet.
+                        @endif
+                    </div>
+                </div>
+            </div>
+
+            <div class="col col-lg-4 col-md-4 col-sm-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        TaskListName and AddTaskButton
+                    </div>
+                    <div class="panel-body">
+                        @if(count($tasks) > 0)
+                            @foreach($tasks as $task)
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        {{$task->name}}
+                                    </div>
+                                    <div class="panel-body">
+                                        <div class="row">
+                                            <div class="col-sm-12">
+                                                <input type="text" form="editTask" name="desc" value="{{$task->desc}}">
+                                            </div>
+                                        </div>
+                                        <form id="editTask" action="{{url('task/' . $task->id)}}" method="POST">
+                                            {{csrf_field()}}
+                                        </form>
+                                        <form id="deleteTask" action="{{url('task/' . $task->id)}}" method="POST">
+                                            {{csrf_field()}}
+                                            {{method_field('DELETE')}}
+                                        </form>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @else
+                            No task yet.
+                        @endif
+                    </div>
+                </div>
+            </div>
+
+            <div class="col col-lg-4 col-md-4 col-sm-6">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         TaskListName and AddTaskButton
