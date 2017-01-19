25df90aba9ee253e7508d47ba8adc8de1ef9c4b0
Hojjat <imani.hojjat95@gmail.com>
2017-01-19 14:04:59 +0330

    added tasklist_id field to tasks migration

---
+++ database/migrations/2017_01_19_103043_add_tasklistid_field_to_task.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTasklistidFieldToTask extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function($table)
+        {
+            $table->string('tasklist_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function ($table){
+            $table->dropColumn('tasklist_id');
+        });
+    }
+}
